223621b206be586a427c05af2f3e3e9e2804cfa1
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2025-03-19 10:43:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //your code here
 
+const btn= document.getElementById("swap")
+const app = document.getElementById("app")
+btn.addEventListener("click", function swapTheme() {
+    app.classList.toggle("day");
+    app.classList.toggle("night");
+    btn.classList.toggle("button_day");
+    btn.classList.toggle("button_night");
+});
